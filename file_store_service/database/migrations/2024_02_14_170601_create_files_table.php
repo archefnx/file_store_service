28caadc1f511a5051d4e07f2cc91e701692b9f64
archefnx <artykbaiev00@mail.ru>
2024-02-14 23:14:43 +0600

    feat: database structure created

--- file_store_service/database/migrations/2024_02_14_170601_create_files_table.php
+++ file_store_service/database/migrations/2024_02_14_170601_create_files_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('files', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
+            $table->string('original_name');
+            $table->string('extension');
+            $table->float('size');
             $table->timestamps();
         });
     }
