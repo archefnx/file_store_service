130ac2011d9ec1a5fdca524015208d74ebeeb5b0
archefnx <artykbaiev00@mail.ru>
2024-02-15 15:39:17 +0600

    feat: routes created for links

--- file_store_service/routes/web.php
+++ file_store_service/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FilesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FilesController::class, 'index'])->name('files.index');
+Route::get('/create', [FilesController::class, 'create'])->name('files.create');
+Route::post('/store', [FilesController::class, 'store'])->name('files.store');
+Route::get('/edit/{id}', [FilesController::class, 'edit'])->name('files.edit');
+Route::put('/update/{id}', [FilesController::class, 'update'])->name('files.update');
+Route::get('/delete/{id}', [FilesController::class, 'delete'])->name('files.delete');
+Route::get('/download/{id}', [FilesController::class, 'download'])->name('files.download');
