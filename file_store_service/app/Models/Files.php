cd6959680a307aae0c55ecf3f85e76aa6a2cb73f
archefnx <artykbaiev00@mail.ru>
2024-02-15 15:42:00 +0600

    feat: database moded connected

--- file_store_service/app/Models/Files.php
+++ file_store_service/app/Models/Files.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Files extends Model
 {
-    protected $fillable = ['name', 'original_name', 'extension', 'size'];
+    protected $fillable = ['name', 'original_name', 'extension', 'size', 'path'];
 }
