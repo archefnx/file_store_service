4ecf00b12fc64b123441a4ccd80306c9180a4f9d
archefnx <artykbaiev00@mail.ru>
2024-02-17 02:31:37 +0600

    refactor: new style flowbite CDN

--- file_store_service/resources/views/layouts/app.blade.php
+++ file_store_service/resources/views/layouts/app.blade.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Документ</title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
 </head>
+
 <body>
-    <main class="py-4">
+
+    <main>
         @yield('content')
     </main>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 </body>
+
 </html>
