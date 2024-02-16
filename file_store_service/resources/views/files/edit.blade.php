a8686c540802660e73204be39a20e610e78cae68
archefnx <artykbaiev00@mail.ru>
2024-02-17 02:30:31 +0600

    refactor: new style added with flowbite

--- file_store_service/resources/views/files/edit.blade.php
+++ file_store_service/resources/views/files/edit.blade.php
@@ -1,27 +1,73 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>Редактирование файла</h2>
 
-    <!-- Форма редактирования файла -->
-    <form method="post" action="{{ route('files.update', ['id' => $file->id]) }}" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
-        <div class="form-group">
-            <label for="name">Название файла:</label>
-            <input type="text" id="name" name="name" class="form-control" value="{{ old('name', $file->name) }}" />
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="{{ url('/') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Docx</span>
+        </a>
+        <button data-collapse-toggle="navbar-default" type="button"
+            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M1 1h15M1 7h15M1 13h15" />
+            </svg>
+        </button>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul
+                class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                    <a href="{{ url('/') }}"
+                        class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                        aria-current="page">Отмена</a>
+                </li>
+            </ul>
         </div>
+    </div>
+</nav>
 
-        <div class="form-group">
-            <label for="file">Файл:</label>
-            <input type="file" id="file" name="file" class="form-control-file" accept="uploads/*" />
-        </div>
+<form class="max-w-sm mx-auto" method="post" action="{{ route('files.update', ['id' => $file->id]) }}" enctype="multipart/form-data" style="padding: 3rem;">
+    @csrf
+    @method('PUT')
+    <div class="mb-5">
+        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Имя файла</label>
+        <input type="text" id="name" name="name" value="{{ old('name', $file->name) }}"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Документ" required />
+    </div>
+    <div class="mb-5">
+        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Загрузить файл</label>
+        <input type="file" id="file" name="file" accept="uploads/*"
+            class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            aria-describedby="file" >
+    </div class="mb-5">
+    <button type="submit"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Сохранять</button>
+</form>
 
-        <!-- Add more form fields as needed -->
-
-        <button type="submit" class="btn btn-primary">Сохранить</button>
-    </form>
-
-    <button><a href="{{ url('/') }}">Cancel</a></button>
+<footer
+    class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="{{ url('/') }}"
+            class="hover:underline">Docx™</a>. Все права защищены.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">О нас</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Конфиденциальность</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Лицензирование</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline">Контакт</a>
+        </li>
+    </ul>
+</footer>
 
 @endsection
