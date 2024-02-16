47430cfbb4ff6e8dac8be2120d7b59c3bb2a39c0
archefnx <artykbaiev00@mail.ru>
2024-02-17 02:44:42 +0600

    docs: files commented

--- file_store_service/app/Models/Files.php
+++ file_store_service/app/Models/Files.php
@@ -7,5 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Files extends Model
 {
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = ['name', 'original_name', 'extension', 'size'];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
 }
