7dadedd7e31807a3bc999f606828382c91d1adea
archefnx <artykbaiev00@mail.ru>
2024-02-17 02:44:08 +0600

    refactor: file name not required

--- file_store_service/resources/views/files/edit.blade.php
+++ file_store_service/resources/views/files/edit.blade.php
@@ -37,7 +37,7 @@
         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Имя файла</label>
         <input type="text" id="name" name="name" value="{{ old('name', $file->name) }}"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Документ" required />
+            placeholder="Документ" />
     </div>
     <div class="mb-5">
         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Загрузить файл</label>
